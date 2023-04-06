40b663fe01b7f89bc676dc63afa1965ca7f01ee8
DashaandKsu <dasha.annenkova06@mail.ru>
2023-04-06 16:50:14 +0300

    update

---
+++ script01.js
@@ -0,0 +1,49 @@
+console.log('что теперь отображается')
+console.log(2**4)
+console.log('вино'+'град')
+console.log(100+'500')
+let username;
+username='Alaska'
+console.log(username)
+let champion;
+champion='France';
+console.log(champion);
+champion='Argentina';
+console.log(champion);
+let a=42;
+let b=42;
+if(a>b){
+    console.log(a);
+}
+else{
+    console.log(b);
+}
+
+if(a && b === 2 , a || b ===2){
+    console.log(a);
+}
+else{
+    console.log(b);
+}
+let winter=['december','january','february']
+console.log(winter[0]);
+
+for(let i=1; i<=20; i=i+2){
+    console.log(i)
+}
+let first=10
+let second=20
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(first,second)
+function sayHello(name){
+    let hello='привет'+ name;
+    return hello
+}
+let key=sayHello('freshMeat')
+console.log(sum)
+
+
+
